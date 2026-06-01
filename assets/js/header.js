5bcc030e543388deea78289f99821c8a85868182
ANKIT KUMAR <ankityadav12044@gmail.com>
2026-06-01 11:41:44 +0530

    add the lead generation fetaure using inquire form

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,6 +5,141 @@ document.querySelectorAll('.loader-wrapper').forEach(loader => {
 });
 
 
+let count = 0;
+
+const percent = document.getElementById("percent");
+const progress = document.getElementById("progress");
+const loader = document.getElementById("loader");
+
+const interval = setInterval(() => {
+
+    count++;
+
+    percent.innerHTML = count + "%";
+    progress.style.width = count + "%";
+
+    if(count >= 100){
+
+        clearInterval(interval);
+
+        setTimeout(() => {
+
+            loader.style.transition = "all .8s ease";
+            loader.style.opacity = "0";
+            loader.style.visibility = "hidden";
+
+        },300);
+
+    }
+
+},20);
+
+
+const leadBox =
+document.getElementById("leadBox");
+
+document
+.getElementById("openLead")
+.onclick = () => {
+
+    leadBox.classList.add("active");
+
+};
+
+document
+.getElementById("closeLead")
+.onclick = () => {
+
+    leadBox.classList.remove("active");
+
+};
+
+
+
+emailjs.init("3C7gGmuSiDjOz0D0V");
+
+const form =
+document.getElementById("quoteForm");
+
+const btn =
+document.getElementById("submit");
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    // 🔵 Button loading start
+    btn.innerHTML = "Sending...";
+    btn.disabled = true;
+
+    emailjs.sendForm(
+        "service_v5rqyta",
+        "template_wznxlu9",
+        this
+    )
+    .then(() => {
+Swal.fire({
+
+    imageUrl:"https://shyamacompany.vercel.app/assets/img/logo.png",
+
+    imageWidth:150,
+
+    title:"Thank You!",
+
+    html:`
+        <p style="font-size:16px;color:#666;">
+            Your quotation request has been submitted.
+        </p>
+
+        <div style="
+            background:#fff7f0;
+            padding:15px;
+            border-radius:12px;
+            margin-top:15px;
+            color:#0a1128;
+            font-weight:600;
+        ">
+            Our team will contact you within 24 hours.
+        </div>
+    `,
+
+    confirmButtonText:"Great!",
+
+    confirmButtonColor:"#ff7a1a",
+
+    background:"#fff",
+
+    backdrop:`
+        rgba(10,17,40,0.7)
+    `
+
+});
+
+        form.reset();
+        // leadBox.classList.remove("active");
+        document.getElementById("leadBox").style.display = "none";
+
+    })
+    .catch((error) => {
+
+        Swal.fire({
+            icon: "error",
+            title: "Failed",
+            text: "Try again later."
+        });
+
+        console.log(error);
+
+    })
+    .finally(() => {
+
+        // 🔵 Button reset
+        btn.innerHTML = "Request Quote";
+        btn.disabled = false;
+
+    });
+
+});
 window.addEventListener("scroll", function () {
   let navbar = document.getElementById("navbar");
 

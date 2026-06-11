00bdc89fa1ca9b789eef7e6e9c0f0efc8d215fa8
ANKIT KUMAR <ankityadav12044@gmail.com>
2026-06-11 19:24:33 +0530

    change the virtual tour feature

--- assets/js/showroom.js
+++ assets/js/showroom.js
@@ -1,21 +1,21 @@
 
-function showRoom(type){
+function showRoom(type) {
 
-document
-.querySelectorAll('.showroom-card')
-.forEach(card=>card.classList.remove('active'));
+    document
+        .querySelectorAll('.showroom-card')
+        .forEach(card => card.classList.remove('active'));
 
-if(type==="bokaro"){
-document
-.getElementById('bokaro-card')
-.classList.add('active');
-}
+    if (type === "bokaro") {
+        document
+            .getElementById('bokaro-card')
+            .classList.add('active');
+    }
 
-if(type==="bihar"){
-document
-.getElementById('bihar-card')
-.classList.add('active');
-}
+    if (type === "bihar") {
+        document
+            .getElementById('bihar-card')
+            .classList.add('active');
+    }
 
 }
 
